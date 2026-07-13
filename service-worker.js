f3d46dce3cd9afc5fe2f2c6f4f12768428b2d204
Vernieman <156662041+verniemanscr@users.noreply.github.com>
2026-07-13 23:00:04 +0800

    Add basic service worker with event listeners

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+});
